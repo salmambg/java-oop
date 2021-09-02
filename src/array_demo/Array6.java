471d07ae7a7e55cdd7fe26ffd822b730063ad51d
salmambg <gsalmahossain@gmail.com>
2021-09-02 21:55:15 +0200

    task: Array output

---
+++ src/array_demo/Array6.java
@@ -0,0 +1,21 @@
+package array_demo;
+
+public class Array6 {
+    public static void main(String[] args) {
+        int[][] number = new int[4][5];
+        int k= 0;
+
+        for (int row=0; row<4;row++) {
+            for(int col=0; col<5;col++) {
+                number[row][col]= k;
+                k++;
+            }
+        }
+        for(int row =0; row<4; row++) {
+            for(int col=0;col<5;col++) {
+                System.out.print(" " +number[row][col]);
+            }
+            System.out.println();
+        }
+    }
+}
